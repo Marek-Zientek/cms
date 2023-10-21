4e92dc42ca8b9b0c3d2173fd376583268cadc58a
Marek Zientek <marek.zientek@drupeo.pl>
2023-10-21 14:21:19 +0200

    Displaying comments based on approval.

--- post.php
+++ post.php
@@ -103,56 +103,45 @@ include "./includes/navigation.php";
             <hr>
 
             <!-- Posted Comments -->
+            <?php
 
-            <!-- Comment -->
-            <div class="media">
-                <a class="pull-left" href="#">
-                    <img class="media-object" src="http://placehold.it/64x64" alt="">
-                </a>
-                <div class="media-body">
-                    <h4 class="media-heading">Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
-                    </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                </div>
-            </div>
 
-            <!-- Comment -->
-            <div class="media">
-                <a class="pull-left" href="#">
-                    <img class="media-object" src="http://placehold.it/64x64" alt="">
-                </a>
-                <div class="media-body">
-                    <h4 class="media-heading">Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
-                    </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    <!-- Nested Comment -->
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Nested Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
-                            </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        </div>
+            $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
+            $query .= "AND comment_status = 'approve' ";
+            $query .= "ORDER BY comment_id DESC ";
+
+            $select_comment_query = mysqli_query($connection, $query);
+
+            if (!$select_comment_query) {
+                die('Query Failed' . mysqli_error($connection));
+            }
+
+            while ($row = mysqli_fetch_assoc($select_comment_query)) {
+                $comment_date = $row['comment_date'];
+                $comment_content = $row['comment_content'];
+                $comment_author = $row['comment_author'];
+
+            ?>
+
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading"><?= $comment_author; ?>
+                            <small><?= $comment_date; ?></small>
+                        </h4>
+                        <?= $comment_content; ?>
                     </div>
-                    <!-- End Nested Comment -->
                 </div>
-            </div>
 
 
-            <!-- Pager -->
-            <!-- <ul class="pager">
-                <li class="previous">
-                    <a href="#">&larr; Older</a>
-                </li>
-                <li class="next">
-                    <a href="#">Newer &rarr;</a>
-                </li>
-            </ul> -->
+
+            <?php } ?>
+
+
+            <!-- Comment -->
+
 
         </div>
 
