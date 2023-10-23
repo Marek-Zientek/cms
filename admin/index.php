78c1e9d636ca6b769b6dc9f2b7ceeda214607bf3
Marek Zientek <marek.zientek@drupeo.pl>
2023-10-23 11:26:27 +0200

    complete page for login system

--- admin/index.php
+++ admin/index.php
@@ -16,8 +16,8 @@ include "includes/admin_header.php";
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="page-header">
-                        Welcome to Admin
-                        <small>Author</small>
+                        Welcome to <?= $_SESSION['user_role']; ?>
+                        <small><?= $_SESSION['username'] ?></small>
                     </h1>
 
                 </div>
